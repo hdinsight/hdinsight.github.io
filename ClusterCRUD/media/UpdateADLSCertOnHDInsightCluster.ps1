ee9076c074a34c0aba20e4f961f8ce4468fb5261
Sunil Kumar Chakrapani <confusionblinds@yahoo.com>
2018-07-27 18:37:21 -0700

    PowerShell Script to update or Create ADL certificate
    Modify some parameters accordingly to choose either existing certificate or create a new one

---
+++ ClusterCRUD/media/UpdateADLSCertOnHDInsightCluster.ps1
@@ -0,0 +1,54 @@
+﻿$clusterName = 'CLUSTERNAME'
+$resourceGroupName = 'RGNAME'
+$subscriptionId = 'SUBSCRIPTIONID'
+$appId = 'APPLICATIONID'
+$generateSelfSignedCert = $false
+$addNewCertKeyCredential = $true
+$certFilePath = 'NEW_CERT_PFX_LOCAL_PATH'
+$certPassword = Read-Host "Enter Certificate Password"
+
+if($generateSelfSignedCert)
+{
+    Write-Host "Generating new SelfSigned certificate"
+    
+    $cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=hdinsightAdlsCert" -KeySpec KeyExchange
+    $certBytes = $cert.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, $certPassword);
+    $certString = [System.Convert]::ToBase64String($certBytes)
+}
+else
+{
+
+    Write-Host "Reading the cert file from path $certFilePath"
+
+    $cert = new-object System.Security.Cryptography.X509Certificates.X509Certificate2($certFilePath, $certPassword)
+    $certString = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes($certFilePath))
+}
+
+
+Login-AzureRmAccount
+
+if($addNewCertKeyCredential)
+{
+    Write-Host "Creating new KeyCredential for the app"
+
+    $keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
+    
+    New-AzureRmADAppCredential -ApplicationId $appId -CertValue $keyValue -EndDate $cert.NotAfter -StartDate $cert.NotBefore
+
+    Write-Host "Waiting for 30 seconds for the permissions to get propagated"
+
+    Start-Sleep -s 30
+}
+
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
+Write-Host "Updating the certificate on HDInsight cluster."
+
+Invoke-AzureRmResourceAction `
+    -ResourceGroupName $resourceGroupName `
+    -ResourceType 'Microsoft.HDInsight/clusters' `
+    -ResourceName $clusterName `
+    -ApiVersion '2015-03-01-preview' `
+    -Action 'updateclusteridentitycertificate' `
+    -Parameters @{ ApplicationId = $appId; Certificate = $certString; CertificatePassword = $certPassword.ToString() } `
+    -Force
